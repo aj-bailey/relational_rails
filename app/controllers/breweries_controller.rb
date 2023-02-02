f8e081f7dd201e163d8ac2351ffd300b94afe818
Adam Bailey <ab67319@gmail.com>
2023-02-02 15:27:16 -0700

    refactor: order brewery index by most recently created

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,8 +1,8 @@
 class BreweriesController < ApplicationController
   def index
-    @breweries = Brewery.all
+    @breweries = Brewery.all.order(created_at: :desc)
   end
-
+`-`
   def show
     @brewery = Brewery.find(params[:id])
   end
