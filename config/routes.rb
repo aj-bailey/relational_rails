b1c62480cd92d6b3503bcc383cb0ae537dab2ea1
Adam Bailey <ab67319@gmail.com>
2023-02-01 17:29:55 -0700

    feat: create beres controller #show, viewer and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/breweries', to: 'breweries#index'
   get '/breweries/:id', to: 'breweries#show'
   get '/beers', to: 'beers#index'
+  get '/beers/:id', to: 'beers#show'
 end
