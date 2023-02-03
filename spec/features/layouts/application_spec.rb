321a9d11072e145ef20acb603a8ab09c5eebe825
Adam Bailey <ab67319@gmail.com>
2023-02-02 18:27:21 -0700

    test: create example 'can see a link at the top of the page that takes me to the Brewery Index'

--- spec/features/layouts/application_spec.rb
+++ spec/features/layouts/application_spec.rb
@@ -6,26 +6,45 @@ RSpec.describe 'Application Pages' do
   # As a visitor
   # When I visit any page on the site
   # Then I see a link at the top of the page that takes me to the Beer Index
-  describe "As a visitor" do
+  describe "As a visitor" do 
     describe "when I visit any page on the site" do
-      it "can see a link at the top of the page that takes me to the Beers Index" do
-        brewery_1 = Brewery.create!(name: "Bonfire Brewing", barrel_program: true, num_taps: 23)
-        beer_1 = Beer.create!(brewery: brewery_1, style: "IPA", abv: 6.9, nitro: false)
+      before(:each) do
+        @brewery_1 = Brewery.create!(name: "Bonfire Brewing", barrel_program: true, num_taps: 23)
+        @beer_1 = Beer.create!(brewery: @brewery_1, style: "IPA", abv: 6.9, nitro: false)
+      end
 
+      it "can see a link at the top of the page that takes me to the Beer Index" do
         visit "/breweries"
         expect(page).to have_link("Beer Index", href: "/beers")
 
         visit "/beers"
         expect(page).to have_link("Beer Index", href: "/beers")
 
-        visit "/breweries/#{brewery_1.id}"
+        visit "/breweries/#{@brewery_1.id}"
         expect(page).to have_link("Beer Index", href: "/beers")
 
-        visit "/beers/#{beer_1.id}"
+        visit "/beers/#{@beer_1.id}"
         expect(page).to have_link("Beer Index", href: "/beers")
 
-        visit "/breweries/#{brewery_1.id}/beers"
+        visit "/breweries/#{@brewery_1.id}/beers"
         expect(page).to have_link("Beer Index", href: "/beers")
+      end
+
+      it "can see a link at the top of the page that takes me to the Brewery Index" do
+        visit "/breweries"
+        expect(page).to have_link("Brewery Index", href: "/breweries")
+
+        visit "/beers"
+        expect(page).to have_link("Brewery Index", href: "/breweries")
+
+        visit "/breweries/#{@brewery_1.id}"
+        expect(page).to have_link("Brewery Index", href: "/breweries")
+
+        visit "/beers/#{@beer_1.id}"
+        expect(page).to have_link("Brewery Index", href: "/breweries")
+
+        visit "/breweries/#{@brewery_1.id}/beers"
+        expect(page).to have_link("Brewery Index", href: "/breweries")
       end
     end
   end
