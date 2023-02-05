b202043ac8d8b7bfec45a42155f08248732e34f9
Adam Bailey <ab67319@gmail.com>
2023-02-04 23:16:22 -0700

    refactor: sort routes for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/beers', to: 'beers#index'
+  get '/beers/:id', to: 'beers#show'
+  get '/beers/:id/edit', to: 'beers#edit'
+
   get '/breweries', to: 'breweries#index'
   get '/breweries/new', to: 'breweries#new'
   get '/breweries/:id', to: 'breweries#show'
-  get '/beers', to: 'beers#index'
-  get '/beers/:id', to: 'beers#show'
   get '/breweries/:brewery_id/beers', to: 'brewery_beers#index'
-  post '/breweries', to: 'breweries#create'
-  get '/breweries/:id/edit', to: 'breweries#edit'
-  patch '/breweries/:id', to: 'breweries#update'
   get '/breweries/:id/beers/new', to: 'brewery_beers#new'
-  post '/breweries/:id/beers', to: 'brewery_beers#create'
-  get '/beers/:id/edit', to: 'beers#edit'
+  get '/breweries/:id/edit', to: 'breweries#edit'
+
   patch '/beers/:id', to: 'beers#update'
+  patch '/breweries/:id', to: 'breweries#update'
+
+  post '/breweries', to: 'breweries#create'
+  post '/breweries/:id/beers', to: 'brewery_beers#create'
 end
