6809cc079190e642b35059c1c1cb37d83a4b2093
Adam Bailey <ab67319@gmail.com>
2023-02-05 11:02:01 -0700

    test: create example test for child index only showing true records

--- spec/features/beers/index_spec.rb
+++ spec/features/beers/index_spec.rb
@@ -13,27 +13,14 @@ RSpec.describe "Beers Index Page", type: :feature do
 
   describe "As a visitor" do
     describe "when I visit '/beers'" do
-      it "can see each beer in the system including the beer's attributes" do 
+      it "can see each beer in the system including the beer's attributes for only nitro beers" do 
         expect(page).to have_content("Beer ID: #{@beer_1.id}")
-        expect(page).to have_content("Beer ID: #{@beer_2.id}")
-
         expect(page).to have_content("Brewery ID: #{@beer_1.brewery_id}")
-        expect(page).to have_content("Brewery ID: #{@beer_2.brewery_id}")
-        
         expect(page).to have_content("Created at: #{@beer_1.created_at}")
-        expect(page).to have_content("Created at: #{@beer_2.created_at}")
-        
         expect(page).to have_content("Updated at: #{@beer_1.updated_at}")
-        expect(page).to have_content("Updated at: #{@beer_2.updated_at}")
-
         expect(page).to have_content("Style: #{@beer_1.style}")
-        expect(page).to have_content("Style: #{@beer_2.style}")
-
         expect(page).to have_content("ABV: #{@beer_1.abv}")
-        expect(page).to have_content("ABV: #{@beer_2.abv}")
-
         expect(page).to have_content("Nitro: #{@beer_1.nitro}")
-        expect(page).to have_content("Nitro: #{@beer_2.nitro}")
       end
     end
   end
--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Beer, type: :model do
   describe 'relationships' do
     it { should belong_to :brewery }
   end
+
+  describe 'class methods' do
+    describe '#list_by_nitro_true' do
+      before(:each) do
+        @brewery_1 = Brewery.create!(name: "Bonfire Brewing", barrel_program: true, num_taps: 23, created_at: Date.new)
+
+        @beer_1 = Beer.create!(nitro: true, style: "Stout", abv: 4.9, brewery: @brewery_1)
+        @beer_2 = Beer.create!(nitro: false, style: "IPA", abv: 6.9, brewery: @brewery_1)
+        @beer_3 = Beer.create!(nitro: true, style: "ESB", abv: 5.3, brewery: @brewery_1)
+      end
+
+      it 'retrieves all beer records where nitro column is true' do
+        expect(Beer.list_by_nitro_true).to eq([@beer_1, @beer_3])
+      end
+    end
+  end
 end
