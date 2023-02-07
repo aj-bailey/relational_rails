46fc0318ed52185ebd517fb2db08d80016d24448
Adam Bailey <ab67319@gmail.com>
2023-02-06 19:18:49 -0700

    test: create feature test for ::partial_match

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Beer, type: :model do
       @brewery_1 = Brewery.create!(name: "Bonfire Brewing", barrel_program: true, num_taps: 23, created_at: Date.new)
 
       @beer_1 = Beer.create!(nitro: true, style: "Stout", abv: 4.9, brewery: @brewery_1)
-      @beer_2 = Beer.create!(nitro: false, style: "IPA", abv: 6.9, brewery: @brewery_1)
-      @beer_3 = Beer.create!(nitro: true, style: "ESB", abv: 5.3, brewery: @brewery_1)
+      @beer_2 = Beer.create!(nitro: false, style: "India Pale Ale", abv: 6.9, brewery: @brewery_1)
+      @beer_3 = Beer.create!(nitro: true, style: "Red Ale", abv: 5.3, brewery: @brewery_1)
     end
 
     describe '::list_by_nitro_true' do
@@ -22,7 +22,7 @@ RSpec.describe Beer, type: :model do
 
     describe '::order_by_alphabetical_style' do
       it 'orders all beer records by alphabetical' do
-        expect(Beer.order_by_alphabetical_style).to eq([@beer_3, @beer_2, @beer_1])
+        expect(Beer.order_by_alphabetical_style).to eq([@beer_2, @beer_3, @beer_1])
       end
     end
 
@@ -35,6 +35,12 @@ RSpec.describe Beer, type: :model do
     describe '::exact_match' do
       it 'retrieves list of beers with exact match for style' do
         expect(Beer.exact_match("Stout")).to eq([@beer_1])
+      end
+    end
+
+    describe '::partial_match' do
+      it 'retrieves list of breweries with partial match for name' do
+        expect(Beer.partial_match("Ale")).to eq([@beer_2, @beer_3])
       end
     end
   end
