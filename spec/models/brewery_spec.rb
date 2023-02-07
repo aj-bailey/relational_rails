8d21bf7def82a5cc2eaf7db991b335508b8705b4
Adam Bailey <ab67319@gmail.com>
2023-02-06 17:26:20 -0700

    test: create model test ::order_by_num_beers_descending

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -17,6 +17,24 @@ RSpec.describe Brewery, type: :model do
         expect(Brewery.order_by_most_recently_created).to eq([@brewery_3, @brewery_2, @brewery_1])
       end
     end
+
+    describe '::order_by_num_beers_ascending' do
+      it 'orders breweries by number of beers in ascending order' do
+        beer_1 = Beer.create!(brewery: @brewery_1, style: "IPA", abv: 6.9, nitro: false)
+        beer_2 = Beer.create!(brewery: @brewery_1, style: "Brown Ale", abv: 5.2, nitro: false)
+
+        beer_3 = Beer.create!(brewery: @brewery_2, style: "IPA", abv: 7.0, nitro: false)
+        beer_4 = Beer.create!(brewery: @brewery_2, style: "Hazy IPA", abv: 6.8, nitro: false)
+        beer_5 = Beer.create!(brewery: @brewery_2, style: "ESB", abv: 5.4, nitro: true)
+
+        beer_9 = Beer.create!(brewery: @brewery_3, style: "IPA", abv: 6.25, nitro: false)
+        beer_10 = Beer.create!(brewery: @brewery_3, style: "Double IPA", abv: 8.0, nitro: false)
+        beer_11 = Beer.create!(brewery: @brewery_3, style: "Sour Brown Ale", abv: 7.0, nitro: false)
+        beer_12 = Beer.create!(brewery: @brewery_3, style: "Pilsner", abv: 5.35, nitro: false)
+
+        expect(Brewery.order_by_num_beers_ascending).to eq([@brewery_3, @brewery_2, @brewery_1])
+      end
+    end
   end
 
   describe 'instance methods' do
